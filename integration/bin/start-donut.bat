6fef0e063fb531728354bb62abacd8b557cfc9be
Evan <dreamcoding@outlook.com>
2017-04-05 19:36:40 +0800

    correct the error of EmbededServer.java

--- integration/bin/start-donut.bat
+++ integration/bin/start-donut.bat
@@ -1,1 +1,5 @@
-java -jar -DDONUT_HOME=.. -Dlog4j.configuration=file:../conf/application-log4j.properties ../server/webapp/web.war ../server/webapp/web.war
+@echo off
+set bin_dir=%~dp0
+set donut_home=%bin_dir%/..
+echo %donut_home%
+java -jar -DDONUT_HOME=%donut_home% -Dlog4j.configuration=file:%donut_home%/conf/application-log4j.properties %donut_home%/server/webapp/web.war %donut_home%/server/webapp/web.war
